9ffad45d761d0103f57d995093493f4ba52e22e3
Susmita <susmita2001singh@gmail.com>
2023-12-13 17:58:23 +0530

    initial layout

---
+++ src/components/DisplayRules.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DisplayRules = () => {
+  return (
+    <>
+      <h1>display rules</h1>
+    </>
+  )
+}
+
+export default DisplayRules
+
---
+++ src/components/UploadRule.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadRule = () => {
+  return (
+    <>
+      <h1>upload rules</h1>
+    </>
+  )
+}
+
+export default UploadRule
