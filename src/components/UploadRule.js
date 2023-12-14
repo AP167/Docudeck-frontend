ebbee7b6740d8be46d9aaed1a2a3966add232696
Susmita <susmita2001singh@gmail.com>
2023-12-14 13:44:54 +0530

    UploadRule frontend

--- src/components/UploadRule.js
+++ src/components/UploadRule.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './styles/styles.css';
 
-const UploadRule = () => {
+const UploadRule = ({ onUpload }) => {
+const [uploadedPDF, setUploadedPDF] = useState(null);
+const [errorMessage, setErrorMessage] = useState('');
+
+  const onDrop = (acceptedFiles) => {
+    const uploadedFile = acceptedFiles[0];
+
+    if (!uploadedFile || uploadedFile.type !== 'application/pdf') {
+      setErrorMessage('*Only PDF files are allowed');
+      setUploadedPDF(null);
+      return;
+    }
+
+    setUploadedPDF(uploadedFile);
+    setErrorMessage('');
+    onUpload(acceptedFiles);
+
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: '.pdf',
+  });
+
   return (
-    <>
-      <h1>upload rules</h1>
-    </>
-  )
-}
+    <div>
+      <div {...getRootProps()} className="dropzone">
+        <div>
+        <h1 className="heading">Upload new Rules & Regulations</h1>
+        <p className='subheading'>(can be memo, circular, notice, etc...)</p>
+        </div>
 
-export default UploadRule
+        <br /> <br /> <br />
+
+        <btn className="mainBtn">Click to select a PDF file</btn>
+        <p className='dropMsg'>or drop PDF here</p>
+  
+        <input {...getInputProps()} />
+        {errorMessage && <p className="error-message"> {errorMessage} </p>}
+        {uploadedPDF && (
+          <div className="uploaded-pdf">
+            <h2>PDF Uploaded.</h2>
+            <p>{uploadedPDF.name}</p>
+            <a href={URL.createObjectURL(uploadedPDF)} target="_blank" >
+                View Uploaded PDF
+              </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UploadRule;
+
+
+
+
+
+
+
